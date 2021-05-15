c53d75d77e38535a102c8da0c61514d027fbebd1
sungbohsun <sungbohsun@gmail.com>
2021-05-15 14:35:58 +0000

    0515

--- test_cnn.py
+++ test_cnn.py
@@ -18,6 +18,7 @@ def test_class(model):
     all_prediction = []
     all_label = []
     for data,lyrics,target in test_loader:
+        target = two_labal(target,args.mode)
         data = data.to(device)
         target = target.to(device)
         output = model(data.to(device))['clipwise_output']
@@ -32,64 +33,116 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--bt", type=int,default=4 ,help="batch size")
     parser.add_argument("--path",help='weight path')
-    parser.add_argument("--mode",  help='Va,Ar',default='4Q')
     args = parser.parse_args()
     
     args.model1 = args.path.split('/')[-2].split('_')[2]
-    args.fold   = args.path.split('/')[2].split('_')[3][-1]
+    args.fold = int(args.path.split('/')[2].split('_')[-1][-1])
     args.CV = args.path.split('/')[-2].split('-')[0]
+    args.mode = args.path.split('/')[-2].split('_')[1]
+        
+    data_size_Bi  = 133 
+    data_size_Q4  = 479
+    data_size_PME = 629
     
     print('--- use Cnn model without lyrics ---')
     pretrain_tk = 'bert-base-uncased'
         
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    test_sets  = []
     #device = 'cpu'
-    all_set = PMEmix_dataset(list(range(629)),pretrain_tk)
-    skf = StratifiedKFold(n_splits=5, shuffle=True,random_state=8848)
-    skf.get_n_splits(all_set)
-    for fold, (train_index, test_index) in enumerate(skf.split(all_set,[y for _,_,y in all_set])):
-        if fold != int(args.fold): continue
-        print('now is in fold',fold)
-        test_set = PMEmix_dataset(test_index,pretrain_tk)
-        kwargs = {'num_workers': 5, 'pin_memory': True} if device == 'cuda' else {} #needed for using datasets on gpu
-        test_loader = DataLoader(test_set, batch_size = args.bt ,shuffle = True, **kwargs)
+    if args.CV == 'PME' :
+        print('CV in PMEmo')
+        all_set = PMEmix_dataset(list(range(data_size_PME)),pretrain_tk)
+        skf = StratifiedKFold(n_splits=5, shuffle=True,random_state=8848)
+        skf.get_n_splits(all_set)
 
-        parms = {'sample_rate':44100,
-         'window_size':1024,
-         'hop_size':320,
-         'mel_bins':64,
-         'fmin':50,
-         'fmax':14000,
-         'classes_num':4 if args.mode == '4Q' else 2}
+        for fold, (train_index, test_index) in enumerate(skf.split(all_set,[y for _,_,y in all_set])):
+#             print(fold,type(fold))
+#             print(args.fold,type(args.fold))
 
-        model = eval(args.model1+'(**parms)') 
-        model.to(device)
-        load_path = './model/{}{}/best_net.pt'.format(args.path.split('/')[-2][:-1],fold)
-        print('load',load_path)
-        model.load_state_dict(torch.load(load_path))
+            if fold != args.fold: continue
 
-        y_true, y_pred = test_class(model)
-        print('Classification Report:')
-        report = classification_report(y_true, y_pred, digits=4)
-        print(report)
+            print('now is in fold',fold)
+            print('-------- load PME')
+            PME_test_set = PMEmix_dataset(test_index,pretrain_tk)
+            test_sets.append(PME_test_set)
 
-        cm = confusion_matrix(y_true, y_pred,list(range(4 if args.mode == '4Q' else 2)))
-        fig = plt.figure(figsize=(10,10),dpi=80)
-        ax = fig.add_subplot(2,1,1)
-        sns.heatmap(cm, annot=True, ax = ax, cmap='Blues', fmt="d")
+    elif args.CV == 'ALL' :
+        print('CV in ALL')
+        all_set = PMEmix_dataset(list(range(data_size_PME)),pretrain_tk)
+        skf = StratifiedKFold(n_splits=5, shuffle=True,random_state=8848)
+        skf.get_n_splits(all_set)
 
-        ax.set_title('{}-Audio_{}_{}_fold{} Confusion Matrix'.format(args.CV,args.mode,args.model1,fold))
+        for fold, (train_index, test_index) in enumerate(skf.split(all_set,[y for _,_,y in all_set])):
 
-        ax.set_xlabel('Predicted Labels')
-        ax.set_ylabel('True Labels')
-        fig.savefig('png/{}-Audio_{}_{}_fold{}.png'.format(args.CV,args.mode,args.model1,fold))
-        
-        f = open('res/{}{}.txt'.format(args.path.split('/')[-2][:-1],fold),'w+')
-        f.write(report)
-        f.close()
-        
-        dic = {}
-        dic['y_true'] = y_true
-        dic['y_pred'] = y_pred
-        torch.save(dic,'predict/{}{}.pt'.format(args.path.split('/')[-2][:-1],fold))
+            if fold != args.fold: continue
+
+            print('now is in fold',fold)
+            print('-------- load PME')
+            PME_test_set = PMEmix_dataset(test_index,pretrain_tk)
+            test_sets.append(PME_test_set)
+
+        all_set = Q4mix_dataset(list(range(data_size_Q4)),pretrain_tk)
+        skf = StratifiedKFold(n_splits=5, shuffle=True,random_state=8848)
+        skf.get_n_splits(all_set)
+
+        for fold, (train_index, test_index) in enumerate(skf.split(all_set,[y for _,_,y in all_set])):
+            if fold != args.fold: continue     
+            print('-------- load Q4')
+            Q4_test_set = Q4mix_dataset(test_index,pretrain_tk)               
+            test_sets.append(Q4_test_set)
+
+        all_set = Bimix_dataset(list(range(data_size_Bi)),pretrain_tk)
+        skf = StratifiedKFold(n_splits=5, shuffle=True,random_state=8848)
+        skf.get_n_splits(all_set)
+
+        for fold, (train_index, test_index) in enumerate(skf.split(all_set,[y for _,_,y in all_set])):
+
+            if fold != args.fold: continue
+            print('-------- load Bi')
+            Bi_test_set = Bimix_dataset(test_index,pretrain_tk)               
+            test_sets.append(Bi_test_set)
+
+    test_set = ConcatDataset(test_sets)
+    kwargs = {'num_workers': 5, 'pin_memory': True} if device == 'cuda' else {} #needed for using datasets on gpu
+    test_loader = DataLoader(test_set, batch_size = args.bt ,shuffle = True, **kwargs)
+
+    parms = {'sample_rate':44100,
+     'window_size':1024,
+     'hop_size':320,
+     'mel_bins':64,
+     'fmin':50,
+     'fmax':14000,
+     'classes_num':4 if args.mode == '4Q' else 2}
+
+    model = eval(args.model1+'(**parms)') 
+    model.to(device)
+    load_path = './model/{}{}/best_net.pt'.format(args.path.split('/')[-2][:-1],args.fold)
+    print('load',load_path)
+    model.load_state_dict(torch.load(load_path))
+
+    y_true, y_pred = test_class(model)
+    print('Classification Report:')
+    report = classification_report(y_true, y_pred, digits=4 if args.mode == '4Q' else 2)
+    print(report)
+
+    cm = confusion_matrix(y_true, y_pred,list(range(4 if args.mode == '4Q' else 2)))
+    fig = plt.figure(figsize=(10,10),dpi=80)
+    ax = fig.add_subplot(2,1,1)
+    sns.heatmap(cm, annot=True, ax = ax, cmap='Blues', fmt="d")
+
+    ax.set_title('{}-Audio_{}_{}_fold{} Confusion Matrix'.format(args.CV,args.mode,args.model1,args.fold))
+
+    ax.set_xlabel('Predicted Labels')
+    ax.set_ylabel('True Labels')
+    fig.savefig('png/{}-Audio_{}_{}_fold{}.png'.format(args.CV,args.mode,args.model1,args.fold))
+
+    f = open('res/{}{}.txt'.format(args.path.split('/')[-2][:-1],args.fold),'w+')
+    f.write(report)
+    f.close()
+
+    dic = {}
+    dic['y_true'] = y_true
+    dic['y_pred'] = y_pred
+    torch.save(dic,'predict/{}{}.pt'.format(args.path.split('/')[-2][:-1],args.fold))
 
--- test_mix.py
+++ test_mix.py
@@ -10,7 +10,7 @@ from tqdm import tqdm
 from torch import nn, optim
 from torch.utils.data import ConcatDataset,TensorDataset
 from sklearn.metrics import f1_score, classification_report, confusion_matrix
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
 import matplotlib.pyplot as plt
 import seaborn as sns
     
@@ -33,25 +33,16 @@ class MIX(nn.Module):
             model_lyr = eval(model_lyrics+'_TL()')
         
         if args.model2 == 'BERT':
-            model_lyr.load_state_dict(torch.load(args.path2))
             self.lyrics = model_lyr.encoder.bert
         
         if args.model2 == 'ALBERT':
-            model_lyr.load_state_dict(torch.load(args.path2))
             self.lyrics = model_lyr.encoder.albert
         
         self.audio  = eval(model_cnn+'(**parms)')
-        
-        if args.model1 == 'Cnn6':
-            self.audio.load_state_dict(torch.load(args.path1))
-        
-        if args.model1 == 'Cnn10':
-            self.audio.load_state_dict(torch.load(args.path1))
-        
         self.audio.fc_audioset = layer_pass()
         self.fc1 = nn.Linear(768+512,768+512)
         self.drop = nn.Dropout(0.5)
-        self.fc2 = nn.Linear(768+512,4)
+        self.fc2 = nn.Linear(768+512,4 if args.mode == '4Q' else 2)
     def forward(self,x1,x2):
         
 #         for param in self.audio.parameters():
@@ -75,6 +66,7 @@ def test_class(model):
     all_prediction = []
     all_label = []
     for batch_idx, (audio,lyrics,target) in enumerate(test_loader):
+        target = two_labal(target,args.mode)
         audio = audio.to(device)
         lyrics = lyrics.to(device)
         target = target.to(device)
@@ -90,20 +82,21 @@ if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
     parser.add_argument("--bt", type=int,default=4 ,help="batch size")
-    parser.add_argument("--path1",help='weight path')
-    parser.add_argument("--path2",help='weight path')
-    parser.add_argument("--mode",  help='Va,Ar',default='4Q')
+    parser.add_argument("--path",help='weight path')
     args = parser.parse_args()
 
     #args.path = './model/Lyr/MIX_Cnn6_ALBERT_l_2_pq/best_net.pt'
-    args.model1 = args.path1.split('/')[-2].split('_')[2]
-    args.model2 = args.path2.split('/')[-2].split('_')[3]
+    args.model1 = args.path.split('/')[-2].split('_')[2]
+    args.model2 = args.path.split('/')[-2].split('_')[3]
     
-    assert args.path1.split('/')[2].split('_')[3][-1] == args.path2.split('/')[2].split('_')[3][-1],'path1 fold != path2 fold'
-    
-    args.fold = int(args.path1.split('/')[2].split('_')[3][-1])
-    args.CV = args.path1.split('/')[-2].split('-')[0]
+    args.fold = int(args.path.split('/')[2].split('_')[-1][-1])
+    args.CV = args.path.split('/')[-2].split('-')[0]
+    args.mode = args.path.split('/')[-2].split('_')[1]
         
+    data_size_Bi  = 133 
+    data_size_Q4  = 479
+    data_size_PME = 629
+    
     if args.model2 == 'BERT':
         pretrain_tk = 'bert-base-uncased'
     
@@ -115,58 +108,106 @@ if __name__ == '__main__':
         pretrain_tk = 'bert-base-uncased'
         
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    test_sets  = []
     #device = 'cpu'
-    all_set = PMEmix_dataset(list(range(629)),pretrain_tk)
-    skf = StratifiedKFold(n_splits=5, shuffle=True,random_state=8848)
-    skf.get_n_splits(all_set)
-    for fold, (train_index, test_index) in enumerate(skf.split(all_set,[y for _,_,y in all_set])):
-        if fold != int(args.fold): continue
-        
-        print('now is in fold',fold)
-        test_set = PMEmix_dataset(test_index,pretrain_tk)
-        kwargs = {'num_workers': 5, 'pin_memory': True} if device == 'cuda' else {} #needed for using datasets on gpu
-        test_loader = DataLoader(test_set, batch_size = args.bt ,shuffle = True, **kwargs)
+    if args.CV == 'PME' :
+        print('CV in PMEmo')
+        all_set = PMEmix_dataset(list(range(data_size_PME)),pretrain_tk)
+        skf = StratifiedKFold(n_splits=5, shuffle=True,random_state=8848)
+        skf.get_n_splits(all_set)
 
-        parms = {'sample_rate':44100,
-         'window_size':1024,
-         'hop_size':320,
-         'mel_bins':64,
-         'fmin':50,
-         'fmax':14000,
-         'classes_num':4 if args.mode == '4Q' else 2}
+        for fold, (train_index, test_index) in enumerate(skf.split(all_set,[y for _,_,y in all_set])):
 
-        model = MIX(args.model1,args.model2) 
-        model.to(device)
+            if fold != args.fold: continue
 
-        model.load_state_dict(torch.load(args.path))
+            print('now is in fold',fold)
+            print('-------- load Lyr_PME')
+            PME_test_set = PMEmix_dataset(test_index,pretrain_tk)
+            test_sets.append(PME_test_set)
 
-        y_true, y_pred = test_class(model)
-        print('Classification Report:')
-        print(classification_report(y_true, y_pred, digits=4))
-         
-        dic = {}
-        dic['y_true'] = y_true
-        dic['y_pred'] = y_pred
-        torch.save(dic,'predict/{}{}.pt'.format(args.path.split('/')[-2][:-1],fold))
+    elif args.CV == 'ALL' :
+        print('CV in ALL')
+        all_set = PMEmix_dataset(list(range(data_size_PME)),pretrain_tk)
+        skf = StratifiedKFold(n_splits=5, shuffle=True,random_state=8848)
+        skf.get_n_splits(all_set)
 
-        cm = confusion_matrix(y_true, y_pred,list(range(4 if args.mode == '4Q' else 2)))
-        fig = plt.figure(figsize=(10,10),dpi=80)
-        ax = fig.add_subplot(2,1,1)
-        sns.heatmap(cm, annot=True, ax = ax, cmap='Blues', fmt="d")
+        for fold, (train_index, test_index) in enumerate(skf.split(all_set,[y for _,_,y in all_set])):
 
-        ax.set_title('{}-MIX_{}_{}_{}_fold{} Confusion Matrix'.format(args.CV,args.mode,args.model1,args.model1,fold))
+            if fold != args.fold: continue
 
-        ax.set_xlabel('Predicted Labels')
-        ax.set_ylabel('True Labels')
+            print('now is in fold',fold)
+            print('-------- load Lyr_PME')
+            PME_test_set = PMEmix_dataset(test_index,pretrain_tk)
+            test_sets.append(PME_test_set)
 
-        fig.savefig('png/{}-MIX_{}_{}_{}_fold{}.png'.format(args.CV,args.mode,args.model1,args.model1,fold))
-        
-        f = open('res/{}{}.txt'.format(args.path.split('/')[-2][:-1],fold),'w+')
-        f.write(report)
-        f.close()
-        
-        dic = {}
-        dic['y_true'] = y_true
-        dic['y_pred'] = y_pred
-        torch.save(dic,'predict/{}{}.pt'.format(args.path.split('/')[-2][:-1],fold))
+        all_set = Q4mix_dataset(list(range(data_size_Q4)),pretrain_tk)
+        skf = StratifiedKFold(n_splits=5, shuffle=True,random_state=8848)
+        skf.get_n_splits(all_set)
+
+        for fold, (train_index, test_index) in enumerate(skf.split(all_set,[y for _,_,y in all_set])):
+            if fold != args.fold: continue     
+            print('-------- load Lyr_Q4')
+            Q4_test_set = Q4mix_dataset(test_index,pretrain_tk)               
+            test_sets.append(Q4_test_set)
+
+        all_set = Bimix_dataset(list(range(data_size_Bi)),pretrain_tk)
+        skf = StratifiedKFold(n_splits=5, shuffle=True,random_state=8848)
+        skf.get_n_splits(all_set)
+
+        for fold, (train_index, test_index) in enumerate(skf.split(all_set,[y for _,_,y in all_set])):
+
+            if fold != args.fold: continue
+            print('-------- load Bi')
+            Bi_test_set = Bimix_dataset(test_index,pretrain_tk)               
+            test_sets.append(Bi_test_set)
+
+    test_set = ConcatDataset(test_sets)
+
+    kwargs = {'num_workers': 5, 'pin_memory': True} if device == 'cuda' else {} #needed for using datasets on gpu
+    test_loader = DataLoader(test_set, batch_size = args.bt ,shuffle = True, **kwargs)
+
+    parms = {'sample_rate':44100,
+     'window_size':1024,
+     'hop_size':320,
+     'mel_bins':64,
+     'fmin':50,
+     'fmax':14000,
+     'classes_num':4 if args.mode == '4Q' else 2}
+
+    model = MIX(args.model1,args.model2) 
+    model.to(device)
+
+    model.load_state_dict(torch.load(args.path))
+
+    y_true, y_pred = test_class(model)
+    print('Classification Report:')
+    report = classification_report(y_true, y_pred, digits=4)
+    print(report)
+
+    dic = {}
+    dic['y_true'] = y_true
+    dic['y_pred'] = y_pred
+    torch.save(dic,'predict/{}{}.pt'.format(args.path.split('/')[-2][:-1],args.fold))
+
+    cm = confusion_matrix(y_true, y_pred,list(range(4 if args.mode == '4Q' else 2)))
+    fig = plt.figure(figsize=(10,10),dpi=80)
+    ax = fig.add_subplot(2,1,1)
+    sns.heatmap(cm, annot=True, ax = ax, cmap='Blues', fmt="d")
+
+    ax.set_title('{}-MIX_{}_{}_{}_fold{} Confusion Matrix'.format(args.CV,args.mode,args.model1,args.model1,args.fold))
+
+    ax.set_xlabel('Predicted Labels')
+    ax.set_ylabel('True Labels')
+
+    fig.savefig('png/{}-MIX_{}_{}_{}_fold{}.png'.format(args.CV,args.mode,args.model1,args.model1,args.fold))
+
+    f = open('res/{}{}.txt'.format(args.path.split('/')[-2][:-1],args.fold),'w+')
+    f.write(report)
+    f.close()
+
+    dic = {}
+    dic['y_true'] = y_true
+    dic['y_pred'] = y_pred
+    torch.save(dic,'predict/{}{}.pt'.format(args.path.split('/')[-2][:-1],args.fold))
 
